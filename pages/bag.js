3ea0ae797019775bf73cfdbb28c7d60c08463b59
Colton Pemberton <czpemberton@gmail.com>
2020-10-21 13:31:57 -0700

    added bag.js

---
+++ pages/bag.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Bag() {
+    return (
+        <div className="container">
+            <Head>
+                <title>Bag</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main></main>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,6 @@ export default function Home() {
                     <div className="block"></div>
                 </div>
             </main>
-
-            <footer></footer>
         </div>
     )
 }
