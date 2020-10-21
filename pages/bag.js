4ebe012e1523af3c0475a932e0b2455cc0827819
Colton Pemberton <czpemberton@gmail.com>
2020-10-21 16:56:22 -0700

    display bag on page

--- pages/bag.js
+++ pages/bag.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import { observer } from 'mobx-react-lite'
 
-export default function Bag() {
+const Bag = ({ bag }) => {
+    const { items, addItem } = bag
     return (
         <div className="container">
             <Head>
@@ -8,7 +10,19 @@ export default function Bag() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main></main>
+            <main>
+                <ul>
+                    {items.map(item => (
+                        <li key={item}>
+                            <span>{item.name} x</span>
+                            <input type="number" value={item.quantity} />
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={() => addItem({ name: 'item' })}>Add Item</button>
+            </main>
         </div>
     )
 }
+
+export default observer(Bag)
