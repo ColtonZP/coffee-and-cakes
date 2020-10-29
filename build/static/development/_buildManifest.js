0ba7ddd3badb8e55863fc33da66d5ab649fcf758
Colton Pemberton <czpemberton@gmail.com>
2020-10-29 14:09:41 -0700

    build

--- build/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/bag":["static\u002Fchunks\u002Fpages\u002Fbag.js"],"/drinks":["static\u002Fchunks\u002Fpages\u002Fdrinks.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fbag","\u002Fdrinks"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
