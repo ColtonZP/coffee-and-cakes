284791021acc3d41d294d3036bb910d5c6c37233
Colton <czpemberton@gmail.com>
2020-11-17 17:52:02 -0800

    Update [drink].js

--- pages/drinks/[drink].js
+++ pages/drinks/[drink].js
@@ -1,12 +1,32 @@
+import Head from 'next/head'
 import { RichText } from 'prismic-reactjs'
 
 import { Client } from '../../lib/prismic-config'
 
-const Drink = ({ item }) => {
+const Drink = ({ item = null }) => {
   return (
-    <>
-      <h1>drink: {RichText.asText(item.data.name)}</h1>
-    </>
+    <div className="container">
+      <Head>
+        <title>{RichText.asText(item.data.name)}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="item">
+        <img src={item.data.picture.url} alt="" />
+        {RichText.render(item.data.name)}
+        {RichText.render(item.data.description)}
+
+        <div className="nutrition">
+          <h3>Nutrition</h3>
+          <span>
+            <b>Calories</b> {item.data.calories}
+          </span>
+          <span>
+            <b>Caffeine</b> {item.data.caffeine}mg
+          </span>
+        </div>
+      </main>
+    </div>
   )
 }
 
