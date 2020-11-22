0c996c655533b4d6a514c5df29bd48888516eb26
Colton <czpemberton@gmail.com>
2020-11-22 10:50:49 -0800

    Fixed discover card regex

--- components/SubmitOrder.js
+++ components/SubmitOrder.js
@@ -122,7 +122,7 @@ const MASTER = {
   name: 'MASTER',
   value: /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/,
 }
-const DISCOVER = { name: 'DISCOVER', value: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/ }
+const DISCOVER = { name: 'DISCOVER', value: /^6(?:011|5[0-9]{2})[0-9]{12}$/ }
 
 const cards = [VISA, AMEX, MASTER, DISCOVER]
 
